1f4b4d4448c5cd3ee504796b41f2ba6e899f1f1f
Andrew Cottam <a.cottam@gmail.com>
2014-05-19 10:46:54 +0200

    water detection updates

--- cgi-bin/createWaterPoints.py
+++ cgi-bin/createWaterPoints.py
@@ -1,7 +1,6 @@
 # Routine to create 10,000 water points from the water detection algorithm so we can get the accuracy of the method
 import sys, json, datetime, math, psycopg2, ee, earthEngine, random
 from dbconnect import dbconnect
-from ee.ee_exception import EEException
 collectionid = 'LANDSAT/LC8_L1T_TOA'
 earthEngine.authenticate() 
 # iterate through the validation sites
@@ -20,44 +19,46 @@ for pathRow in pathRows:
     lat = pathRow[3]
     sceneCount = pathRow[4]
     cloudStats = pathRow[5]
-    print "Getting scenes for " + "long: " + str(lng) + " lat: " + str(lat) + ".."
     scenes = earthEngine.getScenesForPoint(collectionid, lng, lat, 'EPSG:4326')
     mincloud = 100
     mincloudid = ''
-    for scene in scenes['features']:
-        if scene['properties']['WRS_ROW'] == row and scene['properties']['WRS_PATH'] == path:
-            sceneid = scene['properties']["system:index"] 
-            cloud = scene['properties']['CLOUD_COVER']
-            if cloud < mincloud:
-                mincloud = cloud
-                mincloudid = sceneid
+    if len(scenes['features']) > 0:
+        for scene in scenes['features']:
+            if scene['properties']['WRS_ROW'] == row and scene['properties']['WRS_PATH'] == path:
                 sceneSunElevation = scene['properties']['SUN_ELEVATION']
-    sceneid = mincloudid
-    if sceneid:
-        fullsceneid = collectionid + "/" + sceneid 
-        print "Using scene " + fullsceneid 
-        scene = ee.Image(fullsceneid)
-        print "Water detection.."
-        water = earthEngine.detectWater(scene)
-        bbox = scene.geometry().getInfo()['coordinates']
-        thumbnail = water.getThumbUrl({'size': '1000', 'region': bbox, 'min':0, 'max':3, 'palette': '444444,000000,ffffff,0000ff'})
-        print "Water image: " + thumbnail
-        try: 
-            print "Getting random points in bbox " + str(bbox) + ".."
-            random_points = ee.FeatureCollection.randomPoints(scene.geometry(), 1000)
-            print "Getting random points which have been classified as water.."
-            random_points_quantised = water.reduceRegions(random_points, ee.Reducer.first()).filter(ee.Filter.neq('first', None))
-            if len(random_points_quantised.getInfo()['features']) > 0:
-                for feature in random_points_quantised.getInfo()['features']:
-                    coordinate = feature['geometry']['coordinates']
-                    print 'long: ' + str(coordinate[0]) + ' lat: ' + str(coordinate[1]) 
-                    sql2 = "INSERT INTO gee_validated_sites(objectid, gee_lat, gee_lng, predicted_class, sceneid, cloud_cover, sun_elevation,geom) VALUES (" + str(random.randrange(0, 100000000)) + "," + str(coordinate[1]) + "," + str(coordinate[0]) + ",'3','" + fullsceneid + "'," + str(mincloud) + "," + str(sceneSunElevation) + ", ST_SetSRID(ST_Point(" + str(coordinate[1]) + "," + str(coordinate[0]) + "),4326));"
-                    conn.cur.execute(sql2)
-            else:
-                print "No points classified as water"
-        except (EEException) as e:
-            print e.message
-            pass
-        print "\n"
+                if sceneSunElevation > 45:
+                    sceneid = scene['properties']["system:index"] 
+                    cloud = scene['properties']['CLOUD_COVER']
+                    if cloud < mincloud:
+                        mincloud = cloud
+                        mincloudid = sceneid
+        sceneid = mincloudid
+        if sceneid:
+            fullsceneid = collectionid + "/" + sceneid 
+            print "Using scene " + fullsceneid 
+            scene = ee.Image(fullsceneid)
+            print "Water detection"
+            water = earthEngine.detectWater(scene)
+            bbox = scene.geometry().getInfo()['coordinates']
+            thumbnail = water.getThumbUrl({'size': '1000', 'region': bbox, 'min':0, 'max':3, 'palette': '444444,000000,ffffff,0000ff'})
+            print "Water image: " + thumbnail
+            try: 
+                print "Getting random points in bbox " + str(bbox) 
+                random_points = ee.FeatureCollection.randomPoints(scene.geometry(), 1000)
+                print "Getting random points which have been classified as water.."
+                random_points_quantised = water.reduceRegions(random_points, ee.Reducer.first()).filter(ee.Filter.neq('first', None))
+                if len(random_points_quantised.getInfo()['features']) > 0:
+                    for feature in random_points_quantised.getInfo()['features']:
+                        coordinate = feature['geometry']['coordinates']
+                        print 'long: ' + str(coordinate[0]) + ' lat: ' + str(coordinate[1]) 
+                        sql2 = "INSERT INTO gee_validated_sites(objectid, gee_lat, gee_lng, predicted_class, sceneid, cloud_cover, sun_elevation,geom) VALUES (" + str(random.randrange(0, 100000000)) + "," + str(coordinate[1]) + "," + str(coordinate[0]) + ",'3','" + fullsceneid + "'," + str(mincloud) + "," + str(sceneSunElevation) + ", ST_SetSRID(ST_Point(" + str(coordinate[1]) + "," + str(coordinate[0]) + "),4326));"
+                        conn.cur.execute(sql2)
+                else:
+                    print "No points classified as water"
+            except (Exception) as e:
+                pass
+            print "\n"
+        else:
+            print "No scenes found with sun elevation > 45 degrees\n"
     else:
-        print "No scenes found\n"
+        print "No scenes found for path: " + str(path) + " row: " + str(row) + "\n"
